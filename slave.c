a76f723d839d91c73b629353031ce21e4419e175
Bruno Volcovinsky <brunovolko@gmail.com>
2020-09-03 15:11:31 -0300

    Slave comunica minisat -> grep
    Falta comunicar grep -> slave

--- slave.c
+++ slave.c
@@ -27,7 +27,7 @@ int main(int argc, char ** argv){
 
         if(strcmp(input, "EMPTY")!=0){
             // Process the files
-
+        	
             char output[MAX_MESSAGE_LEN];
             process(input, output, slave_id);
 
@@ -45,11 +45,69 @@ int main(int argc, char ** argv){
 }
 
 
-void process(char *input, char *output, int slave_id){
+void process(char *input, char *output, int slave_id) {
+	// Split the input and process each path, then return all answers in CSV format
 
-    printf("@S%d: %s\n", slave_id, input);
-    sprintf(output, "**%s**", input);
+    //rintf("@S%d: %s\n", slave_id, input);
+    //sprintf(output, "**%s**", input);
+    int pipe_fd_sat_grep[2];
+    int pipe_fd_grep_slave[2];
 
-    // Split the input and process each path, then return all answers in CSV format
+    int forkMinisat, forkGrep;    
+	char token[100];
+	token = strtok(input, ",");
+	while(token != NULL) {
+		//Asumo que no hace falta hacer trim, aunque cualquier cosa es sencillo		
+
+		//Pipe para comunicar minisat y grep
+		if(pipe(pipe_fd_sat_grep) == -1) {
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}
+		forkMinisat = fork();
+		if(forkMinisat == 0) {
+			//Minisat escribe
+			close(pipe_fd_sat_grep[0]); //Cierro lo que no uso
+			if (dup2(pipe_fd_sat_grep[1], 1) < 0) {  
+	            perror("dup");
+	            exit(EXIT_FAILURE);
+	        }
+			char *args[] = {"minisat", token, NULL};
+	        if (execv("minisat", args) < 0) {
+	        	perror("exec");
+	            exit(EXIT_FAILURE);
+	        }
+
+		} else if(forkResult == -1) {
+			perror("fork");
+		}
+
+		forkGrep = fork();
+		if(forkGrep == 0) {
+			//grep lee y filtra
+			close(pipe_fd_sat_grep[1]); //Cierro lo que no uso
+			if (dup2(pipe_fd_sat_grep[0], 0) < 0) {  
+	            perror("dup");
+	            exit(EXIT_FAILURE);
+	        }
+			char *args[] = {"grep", "-o", "-e", "Number of .*[0-9]+", "-e", "CPU time.*", "-e", ".*SATISFIABLE", NULL};
+	        if (execv("minisat", args) < 0) {
+	        	perror("exec");
+	            exit(EXIT_FAILURE);
+	        }
+
+		} else if(forkResult == -1) {
+			perror("fork");
+		}
+
+		//Parent, cierro pipe
+		close(pipe_fd_sat_grep[0]);
+		close(pipe_fd_sat_grep[1]);
+
+
+
+
+		token = strtok(NULL, ",");
+	}
 
 }
