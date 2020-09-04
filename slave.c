ce8701c91cd9555383842f1e33ac08ceac9dc313
Bruno Volcovinsky <brunovolko@gmail.com>
2020-09-04 16:43:22 -0300

    Slave funciona, falta pasar resultados a csv
    tambien agregue un archivo cnf que da error para probar

--- slave.c
+++ slave.c
@@ -8,6 +8,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <errno.h>
+
+#define MAX_SIZE_FOR_MINISAT_OUTPUT 2048
+#define MAX_SIZE_FOR_GREP_OUTPUT 512
+
 
 
 void process(char *input, char *output, int slave_id);
@@ -18,6 +23,8 @@ int main(int argc, char ** argv){
     int wr_fd = atoi(argv[1]);
     int rd_fd = atoi(argv[2]);
     int slave_id = atoi(argv[3]);
+
+
 
     while(1){
 
@@ -34,8 +41,7 @@ int main(int argc, char ** argv){
             char output[MAX_MESSAGE_LEN];
             process(input, output, slave_id);
 
-            // Return the result
-            
+            // Return the result            
             write(wr_fd, output, MAX_MESSAGE_LEN);
         }
 
@@ -44,6 +50,7 @@ int main(int argc, char ** argv){
         }
 
     }
+    exit(0);
     
 }
 
@@ -51,69 +58,114 @@ int main(int argc, char ** argv){
 void process(char *input, char *output, int slave_id) {
 	// Split the input and process each path, then return all answers in CSV format
 
-    //rintf("@S%d: %s\n", slave_id, input);
-    //sprintf(output, "**%s**", input);
+    //printf("@S%d: %s\n", slave_id, input);
     int pipe_fd_sat_grep[2];
     int pipe_fd_grep_slave[2];
-    char * inputFromGrep = malloc(200 * sizeof(char));
-    char outputForMaster[200];
-    outputForMaster[0] = 0;
-    int forkMinisat, forkGrep;    
-	char * token = malloc(100 * sizeof(char));//char token[100];
+    char * inputFromGrep = malloc(MAX_SIZE_FOR_GREP_OUTPUT);
+    pid_t forkMinisat, forkGrep;  
+
+	char * token = malloc(100 * sizeof(char)); // token almacena nombre de archivo cnf
 	token = strtok(input, ",");
+
 	while(token != NULL) {
-		//Asumo que no hace falta hacer trim, aunque cualquier cosa es sencillo		
+		//Asumo que no hace falta hacer trim, aunque cualquier cosa es sencillo	
 
 		//Pipe para comunicar minisat y grep
 		if(pipe(pipe_fd_sat_grep) == -1) {
 			perror("pipe");
 			exit(EXIT_FAILURE);
-		}
+		}		
+
+
 		forkMinisat = fork();
 		if(forkMinisat == 0) {
 			//Minisat escribe
 			close(pipe_fd_sat_grep[0]); //Cierro lo que no uso
-			if (dup2(pipe_fd_sat_grep[1], 1) < 0) {  
+
+			if (dup2(pipe_fd_sat_grep[1], 1) < 0) {
 	            perror("dup");
 	            exit(EXIT_FAILURE);
-	        }
+	        }	
+	        
+			
+
+			
+
 			char *args[] = {"minisat", token, NULL};
-	        if (execv("minisat", args) < 0) {
+			printf("proximo: %s\n", token);
+			
+	        if (execv("/usr/bin/minisat", args) < 0) {
 	        	perror("exec");
 	            exit(EXIT_FAILURE);
 	        }
 
+
+
+
+			close(pipe_fd_sat_grep[1]);
+	        exit(0);
+
 		} else if(forkMinisat == -1) {
 			perror("fork");
-		}
-
-		//Pipe para comunicar grep y slave
-		if(pipe(pipe_fd_grep_slave) == -1) {
-			perror("pipe");
 			exit(EXIT_FAILURE);
 		}
+
+		close(pipe_fd_sat_grep[1]); //La escritura acá no la necesito
+		
+		
+
+		
+		
+
+		//Pipe para comunicar grep y slave
+		if(pipe(pipe_fd_grep_slave) == -1) {	
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}		
+
+
 
 		forkGrep = fork();
 		if(forkGrep == 0) { //Proceso que ejcuta grep para filtrar salida del minisat		
 			close(pipe_fd_sat_grep[1]); //Cierro lo que no uso
 			close(pipe_fd_grep_slave[0]); //Cierro lo que no uso
-			if (dup2(pipe_fd_sat_grep[0], 0) < 0 || dup2(pipe_fd_grep_slave[1], 1) < 0) {  
+
+			if (dup2(pipe_fd_grep_slave[1], 1) == -1 || dup2(pipe_fd_sat_grep[0], 0) == -1) {
 	            perror("dup");
 	            exit(EXIT_FAILURE);
 	        }
+
+	        
+
+
 			char *args[] = {"grep", "-o", "-e", "Number of .*[0-9]+", "-e", "CPU time.*", "-e", ".*SATISFIABLE", NULL};
-	        if (execv("minisat", args) < 0) {
+			if (execv("/bin/grep", args) < 0) {
 	        	perror("exec");
 	            exit(EXIT_FAILURE);
 	        }
+	        
+	        close(pipe_fd_sat_grep[0]); //Cierro lo que no uso
+			close(pipe_fd_grep_slave[1]);
+	        exit(0);
 
 		} else if(forkGrep == -1) {
 			perror("fork");
 			exit(EXIT_FAILURE);
 		}
+		//Se cierra lo que no se usa
+		close(pipe_fd_sat_grep[0]);
+		close(pipe_fd_grep_slave[1]);
 
-		//Leo linea por linea lo que imprimio el grep al pipe
-		size_t inputFromGrepSize = 200;
+		read(pipe_fd_grep_slave[0], inputFromGrep, MAX_SIZE_FOR_GREP_OUTPUT);
+		close(pipe_fd_grep_slave[0]);
+
+		int intresult;
+		waitpid(forkMinisat, &intresult, WUNTRACED); //Esperamos a que termine el minisat
+		waitpid(forkGrep, &intresult, WUNTRACED); //Esperamos a que termine el grep
+		printf(inputFromGrep);
+
+		/*
+
 		FILE * fp = fdopen(pipe_fd_grep_slave[0], "r");
 		while(getline(&inputFromGrep, &inputFromGrepSize, fp) > 0) {
 			strcat(outputForMaster, inputFromGrep);
@@ -122,25 +174,16 @@ void process(char *input, char *output, int slave_id) {
 		outputForMaster[strlen(outputForMaster)-1] = '\n';
 
 		if(fclose(fp) != 0) {
+			fwrite("Error 9", 1, strlen("Error 9"), f_debug);
+    		fwrite("\n", 1, 1, f_debug);	
 			perror("fclose");
 			exit(EXIT_FAILURE);
-		}
-        
-
-
-
-		//Parent, cierro pipes
-		close(pipe_fd_sat_grep[0]);
-		close(pipe_fd_sat_grep[1]);
-		close(pipe_fd_grep_slave[0]);
-		close(pipe_fd_grep_slave[1]);
-
-		while ((wait(NULL)) > 0); //Espero a que terminen los 2
-
-		printf("%s", outputForMaster);
+		} */
 
 
 		token = strtok(NULL, ",");
 	}
+
+	free(inputFromGrep);
 
 }
